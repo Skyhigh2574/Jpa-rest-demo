f8e3347e65ffddbad71544e3ab3623a43d80ce46
Skyhigh2574 <skyhigh2574@gmail.com>
2024-01-30 13:46:04 -0600

    Unit Testing of Service Layer and Repository Layer Done

--- src/main/java/com/thinkconstructive/Jparestdemo/service/CloudVendorService.java
+++ src/main/java/com/thinkconstructive/Jparestdemo/service/CloudVendorService.java
@@ -13,4 +13,6 @@ public interface CloudVendorService {
     public CloudVendor getCloudVendor(String cloudVendorId);
     public List<CloudVendor> getAllCloudVendors();
 
+    public List<CloudVendor> getByVendorName(String vendorName);
+
 }
--- src/test/java/com/thinkconstructive/Jparestdemo/service/impl/CloudVendorServiceImplTest.java
+++ src/test/java/com/thinkconstructive/Jparestdemo/service/impl/CloudVendorServiceImplTest.java
@@ -1,18 +1,25 @@
 package com.thinkconstructive.Jparestdemo.service.impl;
 
+import com.thinkconstructive.Jparestdemo.controller.CloudVendorController;
 import com.thinkconstructive.Jparestdemo.model.CloudVendor;
 import com.thinkconstructive.Jparestdemo.repository.CloudVendorRepository;
+import com.thinkconstructive.Jparestdemo.repository.CloudVendorRepositoryTest;
 import com.thinkconstructive.Jparestdemo.service.CloudVendorService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 class CloudVendorServiceImplTest {
@@ -29,7 +36,7 @@ class CloudVendorServiceImplTest {
 
         autoCloseable = MockitoAnnotations.openMocks(this);
         cloudVendorService = new CloudVendorServiceImpl(cloudVendorRepository);
-        cloudVendor =  new CloudVendor("1", "Amazn", "USA", "xxxxxxx");
+        cloudVendor =  new CloudVendor("1", "Amazon", "USA", "xxxxxxx");
     }
 
     @AfterEach
@@ -64,14 +71,55 @@ class CloudVendorServiceImplTest {
     }
 
     @Test
-    void deleteCloudVendor() {
-    }
-
-    @Test
     void getCloudVendor() {
+
     }
 
     @Test
-    void getAllCloudVendors() {
+    void testGetCloudVendor(){
+
+        mock(CloudVendor.class);
+        mock(CloudVendorRepository.class);
+
+        when(cloudVendorRepository.findById("1")).thenReturn(Optional.ofNullable(cloudVendor));
+        assertThat(cloudVendorService.getCloudVendor("1").getVendorName()).isEqualTo(cloudVendor.getVendorName());
+
+    }
+
+    @Test
+    void testGetByVendorName(){
+        mock(CloudVendor.class);
+        mock(CloudVendorRepository.class);
+
+        when(cloudVendorRepository.findByVendorName("Amazon")).thenReturn(
+                new ArrayList<CloudVendor>(Collections.singleton(cloudVendor)
+        ));
+
+                assertThat(cloudVendorService.getByVendorName("Amazon").get(0).getVendorId()).isEqualTo(cloudVendor.getVendorId());}
+
+
+    @Test
+    void testGetAllCLoudVendors(){
+
+        mock(CloudVendor.class);
+        mock(CloudVendorRepository.class);
+
+        when(cloudVendorRepository.findAll()).thenReturn(
+                new ArrayList<CloudVendor>(Collections.singleton(cloudVendor))
+        );
+
+        assertThat(cloudVendorService.getAllCloudVendors().get(0).getVendorPhoneNumber()).isEqualTo(cloudVendor.getVendorPhoneNumber());
+
+    }
+
+    @Test
+    void testDeleteCloudVendor(){
+
+        mock(CloudVendor.class);
+        mock(CloudVendorRepository.class, Mockito.CALLS_REAL_METHODS);
+
+        doAnswer(Answers.CALLS_REAL_METHODS).when(cloudVendorRepository).deleteById(any());
+
+        assertThat(cloudVendorService.deleteCloudVendor("i")).isEqualTo("Success");
     }
 }
